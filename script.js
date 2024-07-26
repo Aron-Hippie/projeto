889c1497da9447942b8f1413999487282835fe98
Aron_Hippie <aron_chucknoia@hotmail.com>
2024-07-26 11:09:15 -0300

    Troca de img, tentativa de troca de nome

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ function toggleMode() {
   //Substituir a Imagem
   if (html.classList.contains("light")) {
     //se tiver lightMode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/Pink-avatar.png")
   } else {
     //se tiver sem o lightMode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Ninja-avatar.png")
+  }
+
+  const CatName = document.querySelector("profile p")
+
+  if (html.classList.contains("light")) {
+    p.setAttribute("Pink")
+  } else {
+    p.setAttribute("Ninja")
   }
 }
