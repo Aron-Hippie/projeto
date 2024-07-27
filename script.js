809563296a870f0af89b55d37760953b85e91cd9
Aron_Hippie <aron_chucknoia@hotmail.com>
2024-07-27 08:49:32 -0300

    tentativas de alteracao no Nome

--- script.js
+++ script.js
@@ -4,21 +4,16 @@ function toggleMode() {
 
   //pegar a tag img
   const img = document.querySelector("#profile img")
+  const catName = document.querySelector("#profile catName")
 
   //Substituir a Imagem
   if (html.classList.contains("light")) {
     //se tiver lightMode, adicionar a imagem light
     img.setAttribute("src", "./assets/Pink-avatar.png")
+    catName.setAttribute("Pink")
   } else {
     //se tiver sem o lightMode, manter a imagem normal
     img.setAttribute("src", "./assets/Ninja-avatar.png")
-  }
-
-  const CatName = document.querySelector("profile p")
-
-  if (html.classList.contains("light")) {
-    p.setAttribute("Pink")
-  } else {
-    p.setAttribute("Ninja")
+    catName.setAttribute("Pink", "Ninja")
   }
 }
